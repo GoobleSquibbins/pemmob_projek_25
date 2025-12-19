ca7bec11cfe1234751b267794866b72dfb2307b9
Eko Susilo <ekosusilo140604@gmail.com>
2025-12-19 14:03:50 +0700

    feat: Add app routing configuration
    - Define all app routes with named routes
    - Support for deep linking and navigation parameters

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String home = '/';
+  static const String nooksList = '/nooks';
+  static const String nookDetail = '/nooks/:id';
+  static const String postDetail = '/posts/:id';
+  static const String createPost = '/create-post';
+}
+
